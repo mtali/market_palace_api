4412866c02e9abc23e9c2e6af5f675f4ee2d68b1
Mtali <emmanuelmtali@yahoo.com>
2017-09-02 16:04:01 +0300

    Add update action on Products controller

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -66,7 +66,46 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
       it { should respond_with 422 }
     end
+  end
 
+  describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token
+    end
 
+    context "when is successfully updated" do
+      before(:each) do
+        patch :update, params: { user_id: @user.id, id: @product.id,
+          product: { title: "An expensive TV"}}
+      end
+
+      it "renders the json representation of updated product" do
+        product_response = json_response
+        expect(product_response[:title]).to eql "An expensive TV"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when not updated" do
+      before(:each) do
+        patch :update, params: { user_id: @user.id, id: @product.id,
+                  product: { price: "two hundred"} }
+      end
+
+      it "render json errors" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json on why the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
   end
 end
