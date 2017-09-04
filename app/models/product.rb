4a1936dd8a16c83100ddfb3e589349b4d4e6d4e5
Mtali <emmanuelmtali@yahoo.com>
2017-09-04 21:26:51 +0300

    Associate products and orders with a placement model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 
+  has_many :placements
+  has_many :orders, through: :placements
+
   belongs_to :user
 
   scope :filter_by_title, lambda { |keyword|
