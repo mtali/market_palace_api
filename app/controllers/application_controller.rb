6b0d021f693969c50ce98c6d308a5c48d97d952a
Mtali <emmanuelmtali@yahoo.com>
2017-09-05 17:39:16 +0300

    Refactor the pagination for index actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   include Authenticable
+
+  protected
+    def pagination(paginates_array, per_page)
+      {
+        pagination: {
+          per_page: per_page.to_i,
+          total_pages: paginates_array.total_pages,
+          total_objects: paginates_array.total_count
+        }
+      }
+    end
 end
